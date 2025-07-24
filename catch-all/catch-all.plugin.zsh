1f556848202a903d0261a723bb8095df44173751
Levi Jumonville <W1ndst0rm@users.noreply.github.com>
2025-07-23 23:04:39 -0600

    Update catch-all.plugin.zsh

--- catch-all/catch-all.plugin.zsh
+++ catch-all/catch-all.plugin.zsh
@@ -1,7 +1,7 @@
 # Replacement Alias
 alias cat="bat"
 alias pcat="bat -pp"
-alias ls="exa"
+alias ls="eza"
 alias df="duf"
 alias du="dust"
 alias curl="curlie"
@@ -11,14 +11,11 @@ alias watch="hwatch"
 if ! command -v abbrev-alias &> /dev/null;
 then
 # General Alias
-alias cdng="code $HOME/Desktop/next-gen.code-workspace"
-alias cdh="code $HOME/Desktop/jville-family.code-workspace"
 alias sshpk="cat .ssh/id_rsa.pub"
 alias sshpkc="cat .ssh/id_rsa.pub | pbcopy"
 alias untar="tar xzvf"
 alias tarup="tar czvf"
 alias grep="grep --color"
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias flushdns="sudo killall -HUP mDNSResponder"
 
 #Pre-Commit Alias
@@ -27,14 +24,11 @@ alias pcin="pre-commit install"
 alias pctr="pre-commit try-repo"
 else
 # General Alias
-abbrev-alias cdng="code $HOME/Desktop/next-gen.code-workspace"
-abbrev-alias cdh="code $HOME/Desktop/jville-family.code-workspace"
 abbrev-alias sshpk="cat .ssh/id_rsa.pub"
 abbrev-alias sshpkc="cat .ssh/id_rsa.pub | pbcopy"
 abbrev-alias untar="tar xzvf"
 abbrev-alias tarup="tar czvf"
 abbrev-alias grep="grep --color"
-abbrev-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 abbrev-alias flushdns="sudo killall -HUP mDNSResponder"
 
 #Pre-Commit Alias
